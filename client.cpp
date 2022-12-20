1ca24fb7df625a22bca111f0de4e97e030a1e5be
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2022-12-20 11:20:22 -0600

    Init commit

---
+++ client.cpp
@@ -0,0 +1,78 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <sstream>
+
+int
+main()
+{
+  // create a socket using TCP IP
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(40000);  // open a socket on port 4000 of the server
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // use localhost as the IP address of the server to set up the socket
+  memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+
+  // connect to the server
+  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+    perror("connect");
+    return 2;
+  }
+
+  struct sockaddr_in clientAddr;
+  socklen_t clientAddrLen = sizeof(clientAddr);
+  if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
+    perror("getsockname");
+    return 3;
+  }
+
+  char ipstr[INET_ADDRSTRLEN] = {'\0'};
+  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+  std::cout << "Set up a connection from: " << ipstr << ":" <<
+    ntohs(clientAddr.sin_port) << std::endl;
+
+
+  // send/receive data (1 message) to/from the server
+  bool isEnd = false;
+  std::string input;
+  char buf[20] = {0};
+  std::stringstream ss;
+
+  while (!isEnd) {
+    memset(buf, '\0', sizeof(buf));
+
+    std::cout << "send: ";
+    std::cin >> input;
+    if (send(sockfd, input.c_str(), input.size(), 0) == -1) {
+      perror("send");
+      return 4;
+    }
+
+
+    if (recv(sockfd, buf, 20, 0) == -1) {
+      perror("recv");
+      return 5;
+    }
+    ss << buf << std::endl;
+    std::cout << "echo: ";
+    std::cout << buf << std::endl;
+
+    if (ss.str() == "close\n")
+      break;
+
+    ss.str("");
+  }
+
+  close(sockfd);
+
+  return 0;
+}
